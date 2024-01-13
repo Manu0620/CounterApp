e5916d7575db194ae94ec66be73702b8d64bc07d
Manuel Madera <maderamanuel25@email.com>
2024-01-13 13:46:36 -0400

    Pruebas a FirstApp realizadas

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { CounterApp } from './CounterApp';
-import './styles.css';
-
 // import { HelloWorldApp } from './HelloWorldApp';
 import { FirstApp } from './FirstApp';
+import './styles.css';
 
-ReactDOM.createRoot( document.getElementById('root') ).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp title='Hola, Manuel' subtitle='Te saluda: ' name='Manuel Madera'/>
+        <FirstApp title='Hola, Manuel' subtitle='Te saluda:' name='Fernando Herrera' />
         {/* <CounterApp value={ 0 } /> */}
     </React.StrictMode>
 )
---
+++ test/FirstApp2.test.jsx
@@ -0,0 +1,56 @@
+import { render, screen } from "@testing-library/react";
+import { FirstApp } from "../src/FirstApp";
+
+describe('Pruebas en <FirstApp />', () => {
+    
+    const title = 'Hola, Manuel';
+    const subtitle = 'Te saluda:';
+    test('debe de hacer match con el snapshot', () => {
+
+        const { container } = render(
+            <FirstApp 
+                title={ title }
+            />
+        )
+
+        expect( container ).toMatchSnapshot();
+        
+    })
+
+    test('debe de mostrar el mensaje "Hola, Manuel"', () => {
+
+        render(
+            <FirstApp
+                title={ title }
+            />
+        )
+
+        expect( screen.getByText( title ) ).toBeTruthy();
+    })
+
+    test('debe de mostrar el titulo en h1', () => {
+
+        render(
+            <FirstApp
+                title={title}
+            />
+        )
+
+        expect( screen.getByRole( 'heading', { level: 1 } ).innerHTML ).toContain( title );
+
+    })
+
+    test('debe de mostrar el subtitulo enviado por props', () => {
+
+        render(
+            <FirstApp
+                title={title}
+                subtitle={subtitle}
+            />
+        )
+
+        expect( screen.getAllByText( subtitle ).length ).toBe( 2 );
+
+    })
+
+});
